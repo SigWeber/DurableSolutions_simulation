f4d1c0e7e6d34d5c2a011c33321ffed5c12aad5a
SigWeber <s.weber.17@ucl.ac.uk>
2022-06-15 09:02:32 +0100

    Revised indicator selection for challenge 2 with new metric option

---
+++ Selected_indicators/colombia_selected_indicators.R
@@ -0,0 +1,262 @@
+library(tidyverse)
+library(haven)
+library(fs)
+
+# Data from: COLOMBIA - Encuesta Nacional de Calidad de Vida - ECV 2019 - http://microdatos.dane.gov.co/index.php/catalog/678/study-description
+# Poverty lines from: Pobreza monetaria en Colombia (Año 2019) - https://www.dane.gov.co/files/investigaciones/condiciones_vida/pobreza/2019/Boletin-pobreza-monetaria_2019.pdf
+
+# preparations ---------------------------------------------------------
+# read data
+datasets <- dir_ls("Data/Colombia-ECV2019", glob = "*.dta") %>% map(compose(as_factor, read_dta))
+
+# bind together datasets with the same unit of analysis
+dwellings <- datasets %>% keep(~all(.$SECUENCIA_P == 1) & all(.$SECUENCIA_ENCUESTA == 1)) %>% reduce(full_join)
+households <- datasets %>% keep(~all(.$SECUENCIA_P == 1) & !all(.$SECUENCIA_ENCUESTA == 1)) %>% reduce(full_join)
+persons <- datasets %>% keep(~!all(.$SECUENCIA_P == 1) & !all(.$SECUENCIA_ENCUESTA == 1)) %>% reduce(full_join)
+
+# harmonize key variables - notice how SECUENCIA_ENCUESTA changes meaning between datasets
+persons <- persons %>% rename(dwelling = DIRECTORIO, household = SECUENCIA_P, person = SECUENCIA_ENCUESTA, WT = FEX_C)
+households <- households %>% rename(dwelling = DIRECTORIO, household = SECUENCIA_ENCUESTA, WT = FEX_C)
+dwellings <- dwellings %>% rename(dwelling = DIRECTORIO, WT = FEX_C)
+
+# identify IDPs
+persons <- 
+  persons %>% 
+  mutate(ID = !str_detect(P756, "otro país") & str_detect(P6096, "(riesgo para su vida|desastre natural)"),
+         ID = replace_na(ID, FALSE))
+
+# identify potential IASC indicators for each subcriteria -------------
+
+# All indicators are coded as 1 if solution "achieved" and zero if not!!!
+
+# 1.1 Victims of violence
+persons <- persons %>% 
+  mutate(
+    # Proportion feeling very or moderately safe walking at night or during the day:
+    # approximated with satisfaction with current level of security
+    I1_SDG_16.1.4 = str_detect(P1898, "([6-9]|'Totalmente satisfecho\\(a\\))"))
+
+
+# 1.2 Freedom of movement
+## Not applicable...
+
+# 2.1. Food security 
+households <- 
+  households %>% 
+  mutate(
+    # Living above the food security line
+    I3_DS_2.1.2  = PERCAPITA > 137350)
+
+# 2.2 Shelter and housing 
+households <- households %>% 
+  mutate(
+    
+    # Security of tenure: Proportion being legally recognized owner of dwelling and having a formal document to proof it
+    I4_SDG_secure_tenure = !str_detect(P5095, "(hecho|colectiva)"),
+    
+    
+    # Structural quality of housing and location: no indicator present on hazardious zones, housing on garbage or in high industrial pollution area
+    
+    #Structural quality of the housing and permanency of the structure:
+ #   SDG_permanency 
+ 
+    # Sufficient living area: Proportion of households in which not more than three people share the same habitable room
+    I4_SDG_sufficient_space = CANT_PERSONAS_HOGAR/P5000 <= 3
+  )
+
+dwellings <- 
+  dwellings %>% 
+  mutate(
+    # Access to improved water sources
+    I4_SDG_improved_water = P8520S5 != "No",
+    
+    # Access to improved sanitation
+    I4_SDG_improved_sanitation = P8520S5 != "No")
+
+
+# 2.3 Medical services 
+persons <- 
+  persons %>% 
+  mutate(
+    # Target population who accessed essential health care services the last time they needed it in the past 12 months
+    # Approximated with Satisfaction with current level of health
+    I5_DS_2.1.8 = str_detect(P1897, "([6-9]|'Totalmente satisfecho\\(a\\))"))
+
+# 2.4 Education
+persons <- 
+  persons %>% 
+  mutate(
+    # SDG indicator 4.1.2: Completion rate (primary education):  School attendance
+    I6_SDG_4.1.2 = P8586 != "No")
+
+# 3.1 Employment and livelihoods
+persons <- 
+  persons %>% 
+  mutate(
+    # Unemployment: Not unemployed
+    I7_SDG_8.5.2 = P6240 != "Buscando trabajo")
+
+# 3.2 Economic security
+households <- 
+  households %>% 
+  mutate(
+    # Living above the national poverty line
+    I8_SDG_8.5.2 = PERCAPITA > 327674)
+
+# 4.1 Property restitution and compensation 
+households <- households %>% 
+  mutate(I9_SDG_1.4.2 = I4_SDG_secure_tenure)
+
+# 5.1. Documentation 
+persons <- 
+  persons %>% 
+  mutate(
+    # DS Library indicator 5.1.1: Target population currently in possession of documentation 
+    I10_DS_5.1.1 = P1894 != "No tiene documento de identidad")
+
+# prepare dataset for simulations -----------------------------------------------
+
+# aggregate individual level data
+# FIXME: we're splitting households to separate IDPs from non-IDPs. Is this the right thing to do?
+colombia_ind_children <- 
+  persons |> 
+  select(ID, dwelling, household, age = P6040, matches("^I6")) |> 
+  filter(between(age, 5, 17)) |> 
+  group_by(ID, dwelling, household) |> 
+  summarize(across(matches("^I6"), compose(as.numeric, any)))
+
+colombia_ind_workingage <- 
+  persons |> 
+  select(ID, dwelling, household, age = P6040, matches("^I(4|7|8)")) |> 
+  filter(between(age, 15, 64)) |> 
+  group_by(ID, dwelling, household) |> 
+  summarize(across(matches("^I(4|7|8)"), compose(as.numeric, any)))
+
+colombia_ind_adults <- 
+  persons |> 
+  select(ID, dwelling, household, age = P6040, matches("^I(1|5|10)")) |> 
+  filter(age >= 15) |> 
+  group_by(ID, dwelling, household) |> 
+  summarize(across(matches("^I(1|5|10)"), compose(as.numeric, any)))
+
+colombia_ind <- 
+  persons |> 
+  distinct(ID, dwelling, household) |> 
+  list(colombia_ind_children, 
+       colombia_ind_adults,
+       colombia_ind_workingage) |> 
+  reduce(left_join)
+
+# build household dataset
+colombia_hh <- 
+  left_join(households %>% select(dwelling, household, starts_with("I"), -starts_with("I_")),
+            dwellings %>% select(dwelling, starts_with("I")))
+
+# merge everything into one dataset
+colombia <- left_join(colombia_hh, colombia_ind)
+
+# combine I4 indicators
+colombia <- colombia %>% 
+  mutate(
+    I4_SDG_11.1.1 = ifelse(rowSums((colombia %>% select(starts_with("I4_SDG_"))) ) < 4, 0, 1)
+  ) %>% 
+  select(-matches("I4_SDG_([a-z])"))
+
+
+# add weighting variable
+colombia <- colombia |> left_join(persons |> count(ID, dwelling, household, wt = WT, name = "WT"))
+
+# add case characteristics for grouping
+colombia <- 
+  colombia %>% 
+  left_join(dwellings |> 
+              transmute(dwelling,
+                        HH_region = REGION,
+                        HH_urban = CLASE == "Cabecera")) |> 
+  left_join(persons |> 
+              filter(P6051 == "Jefe (a) del hogar") |> 
+              transmute(dwelling, household,
+                        HH_head_gender = if_else(P6020 == "Hombre", "Male", "Female"),
+                        HH_years_disp = pmin(P767, 35) |> cut_width(5, boundary = 0),
+                        HH_place_of_origin = P6076S1,
+                        HH_ethnic_minority = P6080 != "Ninguno de los anteriores"))
+
+colombia <- colombia %>% mutate(across(starts_with("HH_"), compose(fct_explicit_na, as_factor)))
+
+# massage data into the right format
+colombia <- colombia %>% mutate(across(where(is.logical), as.numeric))
+
+# welfare-measure
+colombia <- colombia |> left_join(households |> select(dwelling, household, PERCAPITA))
+
+# add case identifier
+colombia <- 
+  colombia %>% 
+  mutate(HHID = str_c(dwelling, household, sep = "-")) %>% 
+  select(-dwelling, -household)
+
+# downsample host community
+colombia <- 
+  bind_rows(colombia %>% filter(ID == 1),
+            colombia %>% filter(ID == 0) %>% slice_sample(n = 5000) %>% mutate(WT = sum(!colombia$ID)/5000*WT))
+
+
+# report difficulty of indicators ----------------------------------------------
+difficulty <- bind_rows(
+  colombia %>% filter(ID==1) %>% select(starts_with("I"),WT) %>% select(-ID) %>% 
+    summarise_all(.,~weighted.mean(., WT, na.rm = TRUE)) %>% mutate(Stat="Difficulty"),
+  colombia %>% filter(ID==1) %>% select(starts_with("I"),WT) %>% select(-ID) %>% 
+    summarise_all(.,~sum(WT*is.na(.))/sum(WT),na.rm=T) %>% mutate(Stat="Missingness")) %>% 
+  pivot_longer(.,cols=c(starts_with("I")),names_to = "Indicator",values_to = "Value") %>% 
+  pivot_wider(.,id_cols = "Indicator",names_from ="Stat",values_from = "Value") %>% 
+  mutate(Difficulty = round(Difficulty*100,2),Missingness = round(Missingness*100,2),
+         order = as.numeric(gsub("I([0-9]+).*$", "\\1", Indicator))) %>% 
+  arrange(order) %>% select(-order)
+#write_csv(difficulty, "Selected_indicators/colombia_difficulty.csv")
+
+# run simulations --------------------------------------------------------------
+source("simulations.R")
+
+run_simulation <- function(data, metric, ..., combinations = NULL) {
+  # divide into IDPs and benchmark
+  idps <- data %>% filter(ID == 1)
+  host_community <- data %>% filter(ID == 0)
+  
+  # identify all possible combinations
+  if (is.null(combinations))
+    combinations <- data %>% extract_indicators() %>% generate_combinations()
+  
+  # draw 500 combinations only - should be enough for all practical purposes
+  combinations <- combinations %>% slice_sample(n = 1, replace=TRUE)
+  
+  # number of IDPs exiting the stock according to the chosen metric
+  Durable_Solutions <- 
+    1:nrow(combinations) %>% 
+    furrr::future_map(metric, data = idps, benchmark = host_community, ..., sim_data = combinations)
+  
+  # result for analysis and plotting
+  combinations %>% 
+    mutate(Durable_Solutions = Durable_Solutions,
+           DS = map_dbl(Durable_Solutions, 
+                        ~left_join(., data, by = "HHID") %>% {sum(.$exited*.$WT, na.rm = TRUE)}),
+           DS_perc = map_dbl(Durable_Solutions, 
+                             ~left_join(., data, by = "HHID") %>% {sum(.$exited*.$WT, na.rm = TRUE)/sum(.$WT)}))
+}
+
+simulation_colombia <- bind_rows(
+  simulate_IRIS_metric(colombia) %>% mutate(metric="Pass/fail"),
+  simulate_composite(colombia)%>% mutate(metric="1: Full composite"),
+  simulate_criterion(colombia)%>% mutate(metric="2: Composite at criterion level"),
+  simulate_subcriterion(colombia)%>% mutate(metric="3: Composite at subcriterion level"),
+  simulate_cells(colombia) %>% 
+    group_by(across(starts_with("I"))) %>% 
+    summarise(DS=mean(DS),DS_perc=mean(DS_perc))%>% mutate(metric="4: Homogeneous cells"),
+  simulate_classifier(colombia)%>% mutate(metric="5: Classifier/regression-based"),
+  simulate_ecdf(colombia) %>% mutate(metric="6: Empirical cumulative density"))%>% 
+  mutate(`Percentage exiting the stock`=round(DS_perc*100,2),
+         `IDP households exiting the stock`=round(DS)) %>% 
+  select(metric,`IDP households exiting the stock`,`Percentage exiting the stock`) %>% 
+  rename(`Metric option`=metric)
+
+#write_csv(simulation_colombia, "Selected_indicators/colombia_selected_indicators.csv")
+
---
+++ Selected_indicators/sudan_selected_indicators.R
@@ -0,0 +1,310 @@
+library(tidyverse)
+
+# preparations ---------------------------------------------------------
+
+# read data
+sudan <- readxl::read_excel("Data/Sudan.xlsx", sheet = 3, na = "NA", guess_max = 15000)
+sudan_ind <- readxl::read_excel("Data/Sudan.xlsx", sheet = 4, na = "NA", guess_max = 15000)
+
+# identify idps vs hosts and add household weights
+sudan <- sudan %>% 
+  rename(ID = migr_idp, WT = weight) %>% 
+  filter(!is.na(ID))
+
+
+# identify potential IASC indicators for each subcriteria -------------
+
+# All indicators are coded as 1 if solution "achieved" and zero if not!!!
+
+# 1.1 Victims of violence
+sudan <- sudan %>% 
+  mutate(
+    # Proportion feeling safe walking at night or during the day
+    I1_SDG_16.1.4 = case_when(
+      H_3_4_safe_walking_night < 3 ~ 1,
+      H_3_5_safe_walking_day < 3 ~ 1,
+      H_3_4_safe_walking_night >= 3 ~ 0,
+      H_3_5_safe_walking_day >= 3 ~ 0,
+      TRUE ~ NA_real_)
+  )
+
+
+# 1.2. Freedom of movement
+# Feeling free to move not covered in questionnaire
+
+
+# 2.1. Food security 
+
+# Food Insecurity Experience Scale (using FAO package)
+
+# identify columns and turn them into binary indicators if any vulnerability appears
+food_insecurity_components <- sudan %>% 
+  select(c("C_4_3_nomoney", 
+           "C_4_4_cop_lessprefrerred",
+           "C_4_5_cop_borrow_food",
+           "C_4_6_cop_limitportion",
+           "C_4_7_cop_limitadult",
+           "C_4_8_cop_reducemeals",
+           "C_4_9_cop_sellassets", 
+           "C_4_10_cop_sellfem")) %>% 
+  summarise_all(~ifelse(.>=1,1,0)) %>% 
+  mutate(I3_DS_2.1.2 = rowSums(.))
+
+sudan$I3_DS_2.1.2 <- (8-food_insecurity_components$I3_DS_2.1.2) # high values are high food security!
+
+# Fixing needed: fit weighted Rasch - I have no idea if and how the Rasch model is broken down to the individual level,
+# currently just use an ordinal scale
+#rr <- RM.w(food_insecurity_components[,1:8],nigeria$WT)
+# pp <- prob.assign(rr, sthres = seq(-5, 5, 0.01))
+
+
+# 2.2 Shelter and housing 
+
+# identify components of sdg indicator: all coded as 1 if NOT vulnerable
+sudan <- sudan %>% 
+  mutate(
+    
+    # Security of tenure: Proportion being legally recognized owner of dwelling and having a formal document to proof it
+    SDG_secure_tenure = case_when(
+      C_1_6_land_legal_main == 1 & C_1_7_land_legal_main_d %in% c(1,2) ~ 1,
+      C_1_6_land_legal_main == 1 & C_1_7_land_legal_main_d %in% 3  ~ 0,
+      C_1_6_land_legal_main == 0  ~ 0,
+      is.na(C_1_6_land_legal_main) == T ~ 0,
+      TRUE ~ NA_real_),
+    
+    # Access to improved water sources
+    SDG_improved_water = case_when(
+      C_1_8_water_home == 1 ~ 1,
+      C_1_8_water_home == 10 ~ 1,
+      C_1_8_water_home == 1000 ~ NA_real_,
+      C_1_8_water_home == 11 ~ 0,
+      C_1_8_water_home == 12 ~ 0,
+      C_1_8_water_home == 2 ~ 1,
+      C_1_8_water_home == 3 ~ 1,
+      C_1_8_water_home == 4 ~ 1,
+      C_1_8_water_home == 5 ~ 1,
+      C_1_8_water_home == 6 ~ 0,
+      C_1_8_water_home == 7 ~ 1,
+      C_1_8_water_home == 9 ~ 1,
+      TRUE ~ NA_real_
+    ),
+    
+    # Access to improved sanitation facilities
+    SDG_improved_sanitation  = case_when(
+      C_1_21_toilet == 1 ~ 1,
+      C_1_21_toilet == 10 ~ 0,
+      C_1_21_toilet == 1000 ~ NA_real_,
+      C_1_21_toilet == 12 ~ 0,
+      C_1_21_toilet == 13 ~ 0,
+      C_1_21_toilet == 2 ~ 1,
+      C_1_21_toilet == 3 ~ 1,
+      C_1_21_toilet == 4 ~ 0,
+      C_1_21_toilet == 5 ~ 1,
+      C_1_21_toilet == 6 ~ 1,
+      C_1_21_toilet == 7 ~ 1,
+      C_1_21_toilet == 8 ~ 0,
+      C_1_21_toilet == 9 ~ 1,
+      TRUE ~ NA_real_
+    ),
+    
+    # Structural quality of housing and location: no indicator present on hazardous zones, housing on garbage or in high industrial pollution area
+    
+    # Structural quality of the housing and permanency of the structure:
+    SDG_permanency = case_when(
+      # living in non-durable (incomplete, not intended, makeshift)
+      C_1_1_housingtype >= 11 ~ 0, 
+      C_1_1_housingtype < 11 ~ 1,
+      # squatting or living in temporary shelter by UNHCR
+      C_1_4_tenure %in% c(5,6,7,8) ~ 0,
+      C_1_4_tenure == 1000  ~ NA_real_,
+      C_1_4_tenure %in% c(1,2,3,4) ~ 1,
+      TRUE ~ NA_real_),
+    
+    # Sufficient living area: Proportion of households in which not more than three people share the same habitable room
+    SDG_sufficient_space = ifelse(household_size/C_1_3_slrooms_n<3, 1, 0)
+  )
+
+# combine to one indicator 
+sudan <- sudan  %>% 
+  mutate(
+    I4_SDG_11.1.1 = ifelse(rowSums((sudan %>% select(starts_with("SDG_"))) == 0) > 0, 0, 1)
+  )
+
+
+# 2.3 Medical services 
+sudan <- sudan %>% 
+  mutate(
+    
+    # Target population who accessed essential health care services the last time they needed it in the past 12 months
+    # Not covered in the questionnaire, approximated with satisfaction with health services
+    I5_DS_2.1.8 = ifelse(H_2_4_health_satisfaction > 3, 0, 1)
+  )
+
+# 2.4 Education 
+sudan_ind <- 
+  sudan_ind |> 
+  
+  # SDG indicator 4.1.2: Completion rate (primary education): not present, 
+  # approximated with whether children ever went to school
+  mutate(
+    # Children went to school
+    I6_SDG_4.1.2  = ifelse(B_3_6_hhm_edu_ever == 0,0,1)
+  )
+
+
+# 3.1 Employment and livelihoods
+sudan_ind <-sudan_ind %>% 
+  mutate(
+    
+    # Unemployment rate:  Proportion engaging in any paid job
+    I7_SDG_8.5.2= ifelse(is.na(B_4_2_1_emp_7d_prim) ==  F,1,0)
+  )
+
+
+# 3.2 Economic security 
+sudan <- sudan %>% 
+  mutate(
+    # Poverty:  Proportion not being poor (190)
+    I8_SDG_8.5.2  = ifelse(poorPPP_190 < 0.5,1,0)
+  )
+         
+         
+# 4.1 Property restitution and compensation /security of tenure
+sudan <- sudan %>% 
+  mutate(
+    # Security of tenure
+    I9_SDG_1.4.2 = SDG_secure_tenure
+  )
+
+
+# 5.1. Documentation 
+sudan_ind <- sudan_ind %>% 
+  mutate(
+    # DS Library indicator 5.1.1: Target population currently in possession of documentation 
+    I10_DS_5.1.1 = ifelse(B_1_13_legal_id == 1,1,0)
+    
+  )
+
+
+# prepare dataset for simulations -----------------------------------------------
+
+# define household characteristics
+sudan <- sudan %>% 
+  mutate(HH_house_type = case_when(
+    C_1_1_housingtype == 1 ~ "Tent",
+    C_1_1_housingtype == 2 ~ "Dwelling of straw mats",
+    C_1_1_housingtype == 3 ~ "Tukul/gottiya - sticks",
+    C_1_1_housingtype == 4 ~ "Tukul/gottiya - mud",
+    C_1_1_housingtype == 5 ~ "Flat or apartment",
+    C_1_1_housingtype == 7 ~ "House of one floor - mud",
+    C_1_1_housingtype == 8 ~ "House of one floor - brick",
+    C_1_1_housingtype == 9 ~ "House constructed of wood",
+    C_1_1_housingtype == 11 ~ "Incomplete structure",
+    TRUE ~ "Other"),
+    
+    HH_year_o_farrival = year_arrival,
+    HH_times_displaced = pmin(I_1_7_disp_site, 3),
+    HH_arrived_with = case_when(
+      I_1_11_disp_arrive_with == 1 ~ "Alone",
+      I_1_11_disp_arrive_with == 2 ~ "With family",
+      I_1_11_disp_arrive_with == 3 ~ "With a larger group but not the family"),
+    HH_community_location = case_when(
+      I_1_16_disp_comm_loc == 1 ~ "Same district",
+      I_1_16_disp_comm_loc == 2 ~ "Same state",
+      I_1_16_disp_comm_loc == 5 ~ "Different state",
+      I_1_16_disp_comm_loc == 6 ~ "Outside Sudan")
+    )
+
+# aggregate individual data
+sudan_ind_children <- 
+  sudan_ind |> 
+  select(household_id, agegrp = B_0_1_hhm_age, starts_with("I6")) |> 
+  filter(agegrp %in% c("06 to 11", "12 to 14", "15 to 17")) |> 
+  group_by(household_id) |> 
+  summarize(across(starts_with("I6"), compose(as.numeric, any, as.logical)))
+
+sudan_ind_workingage <- 
+  sudan_ind |> 
+  select(household_id, agegrp = B_0_1_hhm_age, starts_with("I7")) |> 
+  filter(agegrp %in% c("15 to 17", "18 to 24", "24 to 49", "50 to 66")) |> 
+  group_by(household_id) |> 
+  summarize(across(starts_with("I7"), compose(as.numeric, any, as.logical)))
+
+sudan_ind_all <- 
+  sudan_ind |> 
+  select(household_id, agegrp = B_0_1_hhm_age, starts_with("I10")) |> 
+  group_by(household_id) |> 
+  summarize(across(starts_with("I10")|starts_with("I9"), compose(as.numeric, any, as.logical)))
+
+sudan_ind <- list(sudan_ind_all, sudan_ind_children, sudan_ind_workingage) |> reduce(left_join)
+
+# combine household and individual data
+sudan <- sudan |> left_join(sudan_ind)
+
+# add household ID
+sudan <- sudan %>% mutate(HHID = household_id)
+
+# welfare-measure
+sudan <- sudan |> mutate(PERCAPITA = tc_imp)
+
+# select variables
+sudan <- sudan %>% select(ID, WT, starts_with("HH_"), starts_with("I"), -starts_with("I_"), PERCAPITA, HHID)
+
+
+# report difficulty of indicators ----------------------------------------------
+difficulty <- bind_rows(
+  sudan %>% filter(ID==1) %>% select(starts_with("I"),WT) %>% select(-ID) %>% 
+    summarise_all(.,~weighted.mean(., WT, na.rm = TRUE)) %>% mutate(Stat="Difficulty"),
+  sudan %>% filter(ID==1) %>% select(starts_with("I"),WT) %>% select(-ID) %>% 
+    summarise_all(.,~sum(WT*is.na(.))/sum(WT),na.rm=T) %>% mutate(Stat="Missingness")) %>% 
+  pivot_longer(.,cols=c(starts_with("I")),names_to = "Indicator",values_to = "Value") %>% 
+  pivot_wider(.,id_cols = "Indicator",names_from ="Stat",values_from = "Value") %>% 
+  mutate(Difficulty = round(Difficulty*100,2),Missingness = round(Missingness*100,2)) %>% 
+  mutate(Difficulty = ifelse(Indicator == "I3_DS_2.1.2", NA,Difficulty))
+#write_csv(difficulty, "Selected_indicators/sudan_difficulty.csv")
+
+# run simulations --------------------------------------------------------------
+source("simulations.R")
+run_simulation <- function(data, metric, ..., combinations = NULL) {
+  # divide into IDPs and benchmark
+  idps <- data %>% filter(ID == 1)
+  host_community <- data %>% filter(ID == 0)
+  
+  # identify all possible combinations
+  if (is.null(combinations))
+    combinations <- data %>% extract_indicators() %>% generate_combinations()
+  
+  # draw 500 combinations only - should be enough for all practical purposes
+  combinations <- combinations %>% slice_sample(n = 1, replace=TRUE)
+  
+  # number of IDPs exiting the stock according to the chosen metric
+  Durable_Solutions <- 
+    1:nrow(combinations) %>% 
+    furrr::future_map(metric, data = idps, benchmark = host_community, ..., sim_data = combinations)
+  
+  # result for analysis and plotting
+  combinations %>% 
+    mutate(Durable_Solutions = Durable_Solutions,
+           DS = map_dbl(Durable_Solutions, 
+                        ~left_join(., data, by = "HHID") %>% {sum(.$exited*.$WT, na.rm = TRUE)}),
+           DS_perc = map_dbl(Durable_Solutions, 
+                             ~left_join(., data, by = "HHID") %>% {sum(.$exited*.$WT, na.rm = TRUE)/sum(.$WT)}))
+}
+
+
+simulation_sudan<- bind_rows(
+  simulate_IRIS_metric(sudan) %>% mutate(metric="Pass/fail"),
+  simulate_composite(sudan)%>% mutate(metric="1: Full composite"),
+  simulate_criterion(sudan)%>% mutate(metric="2: Composite at criterion level"),
+  simulate_subcriterion(sudan)%>% mutate(metric="3: Composite at subcriterion level"),
+  simulate_cells(sudan) %>% 
+    group_by(across(starts_with("I"))) %>% 
+    summarise(DS=mean(DS),DS_perc=mean(DS_perc))%>% mutate(metric="4: Homogeneous cells"),
+  simulate_classifier(sudan)%>% mutate(metric="5: Classifier/regression-based"),
+  simulate_ecdf(sudan) %>% mutate(metric="6: Empirical cumulative density"))%>% 
+  mutate(`Percentage exiting the stock`=round(DS_perc*100,2),
+         `IDP households exiting the stock`=round(DS)) %>% 
+  select(metric,`IDP households exiting the stock`,`Percentage exiting the stock`) %>% 
+  rename(`Metric option`=metric)
+
+#write_csv(simulation_sudan, "Selected_indicators/sudan_selected_indicators.csv")
